26b3fdc83d2b01c86fb27ba4bcbc6e15beb1431d
Shaun Hamilton <shauhami020@gmail.com>
2022-03-31 00:23:29 +0100

    chore: add working webview

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,31 @@
-import { ExtensionContext, commands, window } from "vscode";
+import {
+  ExtensionContext,
+  commands,
+  window,
+  ViewColumn,
+  Uri,
+  WebviewPanel,
+} from "vscode";
+import { readFileSync } from "fs";
+import { join } from "path";
 
 export function activate(context: ExtensionContext) {
+  const panel = window.createWebviewPanel(
+    "key-code",
+    "Key Code",
+    ViewColumn.Two,
+    {
+      enableScripts: true,
+      retainContextWhenHidden: true,
+    }
+  );
+
   let disposable = commands.registerCommand("key-code.start", () => {
     window.showInformationMessage("Key Code Activated");
     commands.executeCommand("setContext", "key-code.active", true);
+
+    panel.webview.html = getWebviewContent(context.extensionPath);
+    runLesson(panel);
   });
 
   context.subscriptions.push(
@@ -15,15 +37,46 @@ export function activate(context: ExtensionContext) {
         window.showInformationMessage(`${key}`);
         if (command) {
           commands.executeCommand(command, text);
+          runTests(key);
         }
       }
     )
   );
 
   context.subscriptions.push(disposable);
+
+  function runLesson(panel: WebviewPanel) {
+    // Get lesson content
+    // Post message to panel
+    panel.webview.postMessage({
+      event: "update-description",
+      description: "Hello World",
+    });
+  }
+
+  function runTests(key: string) {
+    // Get tests
+    // If key is correct, pass, and move to next lesson
+    // If key is incorrect, fail.
+    panel.webview.postMessage({
+      event: "update-description",
+      description: key,
+    });
+  }
 }
 
 export function deactivate() {
   commands.executeCommand("setContext", "key-code.active", false);
   window.showInformationMessage("Key Code Deactivated");
+}
+
+/**
+ * Gets the webview/index.html file content
+ */
+function getWebviewContent(extensionPath: string): string {
+  const webviewHTML = Uri.file(
+    join(extensionPath, "src", "webview", "index.html")
+  );
+  const file = readFileSync(webviewHTML.fsPath, "utf8");
+  return file;
 }
