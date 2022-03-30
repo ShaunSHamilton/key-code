81c39195e293c9387eaa8f7e879ff3d68fa772e6
Shaun Hamilton <shauhami020@gmail.com>
2022-03-30 21:19:44 +0100

    chore: init

---
+++ src/extension.ts
@@ -0,0 +1,27 @@
+import { ExtensionContext, commands, window } from "vscode";
+
+export function activate(context: ExtensionContext) {
+  let disposable = commands.registerCommand("key-code.start", (args) => {
+    window.showInformationMessage("Key Code Activated");
+  });
+
+  context.subscriptions.push(
+    commands.registerCommand(
+      "key-code.keybind",
+      ({ key, command, text = "" }) => {
+        // Extension keybinding takes priority over default keybinding
+        // Execute default keybinding
+        window.showInformationMessage(`${key} - ${text}`);
+        if (command) {
+          commands.executeCommand(command, text);
+        }
+      }
+    )
+  );
+
+  context.subscriptions.push(disposable);
+}
+
+export function deactivate() {
+  window.showInformationMessage("Key Code Deactivated");
+}
