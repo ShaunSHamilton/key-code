474b09a298c08d7e7c4351043886cb28a015efa6
Shaun Hamilton <shauhami020@gmail.com>
2022-04-09 22:29:39 +0100

    test: use current directory

--- src/extension.ts
+++ src/extension.ts
@@ -42,8 +42,7 @@ export function activate(context: ExtensionContext) {
     commands.executeCommand("setContext", "key-code.active", true);
 
     panel.webview.html = getWebviewContent(context.extensionPath);
-    const ur = new URL(URI_OF_TEXT_DOCUMENT.toString());
-    appendFile(ur, Buffer.from(""), (err) => {
+    appendFile("./key-code.js", Buffer.from(""), (err) => {
       if (err) {
         window.showErrorMessage(err.message);
       } else {
